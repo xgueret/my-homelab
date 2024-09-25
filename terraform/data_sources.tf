38bcd7c8c9c3af7dadaddc36d4e46d308aa5ba84
xgueret <971xavier.gueret@gmail1.com>
2024-09-25 20:38:41 +0200

    add collaborators management

---
+++ terraform/data_sources.tf
@@ -0,0 +1,3 @@
+data "github_repository" "existing_repo" {
+  full_name = "xgueret/${var.repository_name}"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "github_token" {
+  description = "The GitHub Personal Access Token."
+  type        = string
+  sensitive   = true
+}
+
+variable "repository_name" {
+  description = "The name of the GitHub repository."
+  type        = string
+  default     = "my-homelab"
+}
+
+variable "repository_description" {
+  description = "A description for the GitHub repository"
+  type        = string
+  default     = "Managed by Terraform: HomeLab is a personal project designed to manage and automate a home laboratory environment."
+}
+
+variable "visibility" {
+  description = "The visibility of the GitHub repository (public or private)."
+  type        = string
+  default     = "public"
+}
+
+# Variable to store the list of collaborators with their respective permissions
+variable "collaborators" {
+  type = map(string)
+  description = "A map of collaborators where the key is the GitHub username and the value is the permission level"
+}
